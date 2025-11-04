ff7a37dfd4442f8e73f0ef49c037a2ac9ec3223c
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-11-04 15:59:39 +0000

    Finished first one

--- PG.java
+++ PG.java
@@ -1,1 +1,15 @@
+public class PG{
+    public static void main(String[] args){
 
+        stars(7);
+
+    }
+    public static void stars(int rows){
+            String stars = "";
+            System.out.println("*");
+            for (int i = 1; i <= rows -1; i++){
+                stars += "**";
+                System.out.println(stars);
+            }
+        }
+}
