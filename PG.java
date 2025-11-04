015939fe8d66627836e6b997114ffaf7cf452855
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-11-04 16:09:44 +0000

    Broken - #2

--- PG.java
+++ PG.java
@@ -1,15 +1,51 @@
 public class PG{
     public static void main(String[] args){
 
-        stars(7);
+        //stars(7);
+        triangle(9);
 
     }
+
+
+    public static void triangle(int rows){
+        int num = 1;
+        System.out.println(num);
+        String line = "";
+        int i = 0;
+        while (num <= rows){
+            num++;
+            String sNum = "" + num;
+            while (i < num){
+
+                line += sNum;
+                System.out.println(i);
+            }
+            i = 0;
+            System.out.println(line);
+            line = "";
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     public static void stars(int rows){
             String stars = "";
             System.out.println("*");
-            for (int i = 1; i <= rows -1; i++){
+            int i = 1;
+            while (i <= rows -1){
                 stars += "**";
                 System.out.println(stars);
+                i++;
             }
         }
 }
