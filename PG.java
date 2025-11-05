e3d1417358bfb7c7e3a6f87007eb079ef2bcbb2c
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-11-05 13:01:30 +0000

    Progress = Good

--- PG.java
+++ PG.java
@@ -2,28 +2,18 @@ public class PG{
     public static void main(String[] args){
 
         //stars(7);
-        triangle(9);
+        //triangle(9);
+        //odds(9);
+        eo(6);
 
     }
 
 
-    public static void triangle(int rows){
-        int num = 1;
-        System.out.println(num);
-        String line = "";
-        int i = 0;
-        while (num <= rows){
-            num++;
-            String sNum = "" + num;
-            while (i < num){
 
-                line += sNum;
-                System.out.println(i);
-            }
-            i = 0;
-            System.out.println(line);
-            line = "";
+    public static void eo(int maxE){
+        if (maxE % 2 == 0){
             
+
         }
     }
 
@@ -38,8 +28,56 @@ public class PG{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void odds(int start){
+        String sOdd = "";
+        for (int i = start; i > 0; i-=2){
+            for (int j = 0; j < i; j++){
+                sOdd += i;
+            }
+            System.out.println(sOdd);
+            sOdd = "";
+        }
+
+    }
+
+    public static void triangle(int rows){
+        int num = 1;
+        System.out.println(num);
+        String line = "";
+        int i = 0;
+        while (num < rows){
+            num++;
+            String sNum = "" + num;
+            while (i < num){
+
+                line += sNum;
+                i++;
+            }
+            i = 0;
+            System.out.println(line);
+            line = "";
+            
+        }
+    }
+
     public static void stars(int rows){
-            String stars = "";
+            String stars = "*";
             System.out.println("*");
             int i = 1;
             while (i <= rows -1){
